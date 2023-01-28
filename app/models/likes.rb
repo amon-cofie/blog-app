5f3d16793360712a4a3f8a4253e4e885cf4a4ebd
amon-cofie <ddd.amon123@gmail.com>
2023-01-28 01:30:28 -0800

    Create Like action record

---
+++ app/models/likes.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+    belongs_to :author, class_name: "User"
+    belongs_to :post
+    after_create :update_likes_counter
+
+    def update_likes_counter
+        post_id = post.id
+        Post.increment_counter(:likes_counter, post_id)    
+    end
+end
