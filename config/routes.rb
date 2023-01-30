10c7437f68e31aabd709dc27838d002cc3c83ad1
amon-cofie <ddd.amon123@gmail.com>
2023-01-30 06:31:07 -0800

    Set up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pages#home"
+  
+  resources :users, only: [:show] do
+    resources :posts, only: [:index, :show]
+  end
+  
 end
